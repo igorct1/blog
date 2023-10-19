e98f2bd8923bc3b43b2cd475e45553b4b504812a
igorct1 <igorct77@gmail.com>
2023-10-19 16:47:32 -0300

    feat: adding gem friendly_id to manage our dynamics paths

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,9 @@
 class Post < ApplicationRecord
+  extend FriendlyId
   validates :title, :author, :body, presence: true
 
   # associação -> relacionamento de post com comentários
   has_many :comments
+
+  friendly_id :title, use: :slugged
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_18_145044) do
+ActiveRecord::Schema.define(version: 2023_10_19_193541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2023_10_18_145044) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
   end
 
   add_foreign_key "comments", "posts"
